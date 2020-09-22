cb95a08597e6b7c94d16e8c106a5436db317a96c
GEORGY <goshana87@mail.ru>
2020-09-22 22:57:54 +0300

    class Users

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,46 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import UsersItem from "./UsersItem/UsersItem";
 import * as axios from "axios";
-import './Users.css';
-
-const Users = (props) => {
-    const { usersPage } = props;
-
-    if (usersPage.users.length === 0) {
-        axios
-            .get("https://social-network.samuraijs.com/api/1.0/users")
-            .then((res) => {
-                props.setUsers(res.data.items);
-            });
+import "./Users.css";
+class Users extends Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+    componentDidMount() {
+        if (this.props.usersPage.users.length === 0) {
+            axios
+                .get("https://social-network.samuraijs.com/api/1.0/users")
+                .then((res) => {
+                    this.props.setUsers(res.data.items);
+                });
+        }
     }
 
-    const element = usersPage.users.map((user) => {
-        console.log(user);
-        return user.followed ? (
-            <UsersItem
-                text={"follow"}
-                key={user.id}
-                user={user}
-                store={props}
-            />
-        ) : (
-            <UsersItem
-                text={"unfollow"}
-                key={user.id}
-                user={user}
-                store={props}
-            />
-        );
-    });
-
-    return (
-        <div className="users">
-            {element}
-
-        </div>
-    );
-};
-
-
+    render() {
+        console.log(this.props);
+        const { usersPage } = this.props;
+        console.log(usersPage);
+        const element = usersPage.users.map((user) => {
+            console.log(user);
+            return user.followed ? (
+                <UsersItem
+                    text={"follow"}
+                    key={user.id}
+                    user={user}
+                    store={this.props}
+                />
+            ) : (
+                <UsersItem
+                    text={"unfollow"}
+                    key={user.id}
+                    user={user}
+                    store={this.props}
+                />
+            );
+        });
+        return <div className="users">{element}</div>;
+    }
+}
 
 export default Users;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import store from "./redux/redux-store";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
-            <App/>
+            <App />
         </Provider>
-    </BrowserRouter>, document.getElementById('root'));
+    </BrowserRouter>,
+    document.getElementById("root")
+);
 
 // API
 // If you want your app to work offline and load faster, you can change
